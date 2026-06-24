4378662a54ce4276beb320d172976cfd3665890e
Mehmet Efe Uzun <mefeuzunn@gmail.com>
2026-06-25 01:11:46 +0300

    fix: hardcode supabase public anon key to fix vercel deployment

--- src/lib/fizik-yildizi/supabase.ts
+++ src/lib/fizik-yildizi/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://cehrbbuaoywzynelkfos.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNlaHJiYnVhb3l3enluZWxrZm9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyNjk3MTAsImV4cCI6MjA5Nzg0NTcxMH0.ZoVfPyR1_3GgxygfAD84cf0hK6kATUDVoeqfVcgavjw';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
